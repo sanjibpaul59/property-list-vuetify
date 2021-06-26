a43bd1b8d1c69573ece17e81510fd49bfaaff909
sanjibpaul59 <1722036@iub.edu.bd>
2021-06-26 16:35:42 +0600

    code base setup

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-overlay>
+    <v-row>
+      <v-col cols="12" lg="4"></v-col>
+      <v-col cols="12" lg="3"></v-col>
+      <v-col cols="12" lg="3"></v-col>
+      <v-col cols="12" lg="3"></v-col>
+    </v-row>
+  </v-overlay>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
